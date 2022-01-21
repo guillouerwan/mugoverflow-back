8d6369f6b52f2516bfeef19ee459fef9a7852f34
Renega59 <nicolas.lenne@outlook.fr>
2022-01-21 12:07:23 +0100

    ajout commentaires

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -21,20 +21,18 @@ class AppFixtures extends Fixture
 
     public function __construct(Connection $connection)
     {
-        // On récupère la connexion à la BDD (DBAL ~= PDO)
-        // pour exécuter des requêtes manuelles en SQL pur
+        // Get connection of mysql BDD
         $this->connection = $connection;
     }
 
     /**
-     * Permet de TRUNCATE les tables et de remettre les AI à 1
+     * Set AI to 1 in BDD table and Truncate
      */
     private function truncate()
     {
-        // On passe en mode SQL ! On cause avec MySQL
-        // Désactivation la vérification des contraintes FK
+
         $this->connection->executeQuery('SET foreign_key_checks = 0');
-        // On tronque
+
         $this->connection->executeQuery('TRUNCATE TABLE category');
         $this->connection->executeQuery('TRUNCATE TABLE main_color');
         $this->connection->executeQuery('TRUNCATE TABLE product');
@@ -48,21 +46,22 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // On TRUNCATE manuellement
+        // Truncate manualy
         $this->truncate();
 
         // use the factory to create a Faker\Generator instance
         $faker = Faker\Factory::create('fr_FR');
 
-        // Pour avoir toujours les mêmes données (le même hasard)
+        // to get everytime the same data
         $faker->seed(2021);
 
-        // Notre data Provider
+        // Our own data provider
         $MugOverflowProvider = new MugOverflowProvider();
-        // On le fournit à Faker
+
+        // We set Faker with out date provider
         $faker->addProvider($MugOverflowProvider);
 
-        // Création d'un user :
+        // Create 3 users :
         // superadmin
         $superAdmin = new User();
         $superAdmin->setEmail('superadmin@superadmin.com');
@@ -97,43 +96,42 @@ class AppFixtures extends Fixture
 
         // Promo
 
-        // Tableau de nos promos
+        // Promo data array
         $promosList = [];
 
         for ($i = 1; $i <= 4; $i++) {
 
-            // Nouvelle catégorie
+            // New promo
             $promo = new Promo();
             $promo->setName($faker->unique()->promoTitle());
             $promo->addUser($userList[mt_rand(0,2)]);
 
-            // On l'ajoute à la liste pour usage ultérieur
+            // Add promo to the array
             $promosList[] = $promo;
 
-            // On persiste
             $manager->persist($promo);
         }
 
         // Catégory
 
-        // Tableau de nos catégory
+        // Category data array
         $categoriesList = [];
 
         for ($i = 1; $i <= 6; $i++) {
 
-            // Nouvelle catégorie
+            // New Category
             $category = new Category();
             $category->setName($faker->unique()->productCategory());
 
-            // On l'ajoute à la liste pour usage ultérieur
+            // Add the category to the array
             $categoriesList[] = $category;
 
-            // On persiste
             $manager->persist($category);
         }  
 
 
         // MainColor
+        // Set a main color
         $mainColor = new MainColor();
         $mainColor->setMainColorName('White');
         $mainColor->setMainHexa('#FFFFFF');
@@ -142,6 +140,7 @@ class AppFixtures extends Fixture
         $manager->persist($mainColor);
 
         // SecondaryColor
+        // Set a secondary color
         $secondaryColor = new SecondaryColor();
         $secondaryColor->setSecondaryColorName('Black');
         $secondaryColor->setSecondaryHexa('#000000');
@@ -151,6 +150,7 @@ class AppFixtures extends Fixture
 
         // Product
         for ($i=1; $i < 10; $i++) { 
+            // Create new product
             $product = new Product();
             $product->setName($MugOverflowProvider->mugTitle(mt_rand(0, 9)));
             $product->addUser($userList[mt_rand(0,2)]);
@@ -158,24 +158,22 @@ class AppFixtures extends Fixture
             $product->setSecondaryColor($secondaryColor);
             $product->setCreatedAt(new DateTime());
             $product->setStatus(mt_rand(0,1));
+            // Set random paragraph
             $product->setDescription($faker->paragraph(2));
+            // Set images
             $product->setLogo('https://s3-eu-west-1.amazonaws.com/tpd/logos/595cae450000ff0005a600d6/0x0.png');
             $product->setImage('https://picsum.photos/id/'.mt_rand(1, 100).'/300/300');
-
             $product->setMockupBack('https://cdn.habitat.fr/thumbnails/product/1319/1319267/box/850/850/40/F4F4F4/mug-8cm-en-porcelaine-blanche_1319267.jpg');
             $product->setMockupFront('https://ih1.redbubble.net/image.1791840745.7490/ur,mug_lifestyle,tall_portrait,750x1000.jpg');
             
-
-            // On ajoute de 1 à 3 catégorie par produit
+            // We add random number category by product
             for ($g = 1; $g <= mt_rand(1, 3); $g++) {
-
                 $randomCategories = $categoriesList[mt_rand(0, count($categoriesList) - 1)];
                 $product->addCategory($randomCategories);
                 
             }
 
             $manager->persist($product);
-
 
         }
 
