1912b3e968151198c18d0fc74f96bda27dce8453
Renega59 <nicolas.lenne@outlook.fr>
2022-01-21 19:15:40 +0100

    méthode de la récupération du profil en cours

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserController extends AbstractController
 {
@@ -10,13 +14,16 @@ class UserController extends AbstractController
     /**
      * Get the profil of the user authenticated 
      * 
-     * @Route("/api/profil", name="api_user_profil", methods={"GET})
+     * @Route("/api/profil", name="api_user_profil", methods={"GET"})
      */
-    public function getProfil()
+    public function getProfil(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager): Response
     {
+
+        $decodedJwtToken = $jwtManager->decode($tokenStorageInterface->getToken());
+
         return $this->json(
             // Les données à sérialiser (à convertir en JSON)
-            ['test' => 'test'],
+            $decodedJwtToken,
             // Le status code
             200,
             // Les en-têtes de réponse à ajouter (aucune)
