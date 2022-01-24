c0487abaec7a6451c90892eef705a4f46e62ed4b
Renega59 <nicolas.lenne@outlook.fr>
2022-01-24 17:08:01 +0100

    register function done

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Promo;
 use App\Entity\User;
 use App\Form\RegisterType;
 use App\Form\EditProfilType;
+use App\Repository\PromoRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\JsonLoginFactory;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -68,10 +71,17 @@ class UserController extends AbstractController
      * 
      * @Route("/api/register", name="api_user_register", methods={"POST"})
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface,SerializerInterface $serializer, ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface,SerializerInterface $serializer, ManagerRegistry $doctrine, ValidatorInterface $validator, PromoRepository $promoRepository): Response
     {
-
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $jsonContent = json_decode($request->getContent(), true);
+
+        $promo = $promoRepository->find($jsonContent["promo"]);
+
+        if(!$promo){
+            return $this->json("La promo saisie n'existe pas.", 500);
+        }
 
         $errors = $validator->validate($user);
 
@@ -80,10 +90,11 @@ class UserController extends AbstractController
         }
 
         $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $user->getPassword());
-        // On écrase le mot de passe en clair par le mot de passe haché
+
         $user->setPassword($hashedPassword);
 
-        // On sauvegarde l'entité
+        $promo->addUser($user);
+
         $em = $doctrine->getManager();
         $em->persist($user);
         $em->flush();
