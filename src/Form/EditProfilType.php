40e44d0abb76e33e6d78045a74360de0eeae4952
Renega59 <nicolas.lenne@outlook.fr>
2022-02-01 16:31:16 +0100

    ajout de modification du status et promo dans le profil

--- src/Form/EditProfilType.php
+++ src/Form/EditProfilType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Promo;
+use App\Entity\Status;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,6 +18,12 @@ class EditProfilType extends AbstractType
             ->add('email')
             ->add('firstname')
             ->add('lastname')
+            ->add('status', EntityType::class, [
+                'class' => Status::class
+            ])
+            ->add('promo', EntityType::class, [
+                'class' => Promo::class
+            ])
         ;
     }
 
