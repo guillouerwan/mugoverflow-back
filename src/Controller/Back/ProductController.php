060d6256a67fabe29fdda891ec8648f83994c75e
Renega59 <nicolas.lenne@outlook.fr>
2022-02-01 13:59:51 +0100

    modification du nombre de produit à l'affichage

--- src/Controller/Back/ProductController.php
+++ src/Controller/Back/ProductController.php
@@ -27,13 +27,12 @@ class ProductController extends AbstractController
      */
     public function index(ProductRepository $productRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        // Méthode findBy qui permet de récupérer les données avec des critères de filtre et de tri
         $donnees = $productRepository->findAll();
 
         $products = $paginator->paginate(
-            $donnees, // Requête contenant les données à paginer (ici nos articles)
-            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-            6 // Nombre de résultats par page
+            $donnees, 
+            $request->query->getInt('page', 1), 
+            5 
         );
 
         return $this->render('back/product/index.html.twig', [
